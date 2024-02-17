9d39b41bca2cbf7dd8860180f912b143d950140c
aerixyariel <157715942+aerixyariel@users.noreply.github.com>
2024-02-17 21:47:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,35 @@ const mc = require('minecraft-protocol')
 
 var client = null;
 
-http.createServer(function (req, res) {
+function reConnect(res) {
   client = mc.createClient({
     host: 'samudra.aternos.me',
     port: '64210',
     username: 'Bot',
     auth: 'offline',
   })
-  console.log(`Just got a request at ${req.url}!`)
-  res.write('Yo!');
+
+  client.on('error', function (err) {
+    res.write(err)
+  })
+
+  client.on('connect', function () {
+    res.write('Connected !')
+  })
+  client.on('disconnect', function (packet) {
+    res.write('Disconnected: ' + packet.reason)
+    res.write('Re-connecting....')
+    reConnect(res);
+  })
+  client.on('end', function () {
+    res.write('Connection lost !')
+    res.write('Re-connecting....')
+    reConnect(res);
+  })
+}
+
+http.createServer(function (req, res) {
+  res.write('Connecting....')
+  reConnect(res);
   res.end();
 }).listen(process.env.PORT || 3000);
